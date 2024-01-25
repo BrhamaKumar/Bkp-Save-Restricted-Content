dd7c8f44b24107ffb9d05c8114f06a4e19eabc0e
BrhamaKumar <146445615+BrhamaKumar@users.noreply.github.com>
2024-01-25 15:42:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,3 +6,7 @@ class Config(object):
 
     TECH_VJ_URL = environ.get('TECH_VJ_URL', 'moneykamalo.com') # your shortlink url domain or url without https://
     TECH_VJ_API = environ.get('TECH_VJ_API', '0eefb93e1e3ce9470a7033115ceb1bad13a9d674') # your url shortner api
+    
+    # channel information
+    TECH_VJ_LOG_CHANNEL = int(os.environ.get("TECH_VJ_LOG_CHANNEL", "")) # your log channel id and make bot admin in log channel with full right 
+    
